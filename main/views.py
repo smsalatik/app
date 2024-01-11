d7f117d9bf26ad641397f1916cc7a78b0a5dc913
smsalatik <smsalikh@mail.ru>
2024-01-11 14:40:22 +0300

    After adding fixtures for products

--- main/views.py
+++ main/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
+from products.models import Categories
+
 def index(request):
+    categories = Categories.objects.all()
     context = {
         'title': 'DAGMeet - Главная',
         'content': 'Халяльная продукция с Дагестана DAGMeet',
+        'categories': categories,
     }
 
     return render(request, 'main/index.html', context)
--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from products.models import Categories, Products
+
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
