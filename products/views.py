96c34329ec844542a2483e455712d1ae1763bd32
smsalatik <smsalikh@mail.ru>
2024-02-04 21:00:13 +0300

    Slug converter for product

--- products/views.py
+++ products/views.py
@@ -13,5 +13,11 @@ def catalog(request):
     return render(request, "products/catalog.html", context)
 
 
-def product(request):
-    return render(request, "products/product.html")
+def product(request, product_slug):
+
+    product = Products.objects.get(slug=product_slug)
+
+    context = {
+        'product': product
+    }
+    return render(request, "products/product.html", context=context)
