c8047eebcbae382622bd2ec6c7d5136f5bd0af1b
smsalatik <smsalikh@mail.ru>
2024-03-11 13:52:28 +0300

    Simple search created

--- products/views.py
+++ products/views.py
@@ -2,15 +2,18 @@ from django.core.paginator import Paginator
 from django.shortcuts import get_list_or_404, render
 
 from products.models import Products
+from products.utils import q_search
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug=None):
     page = request.GET.get('page', 1)
     order_by = request.GET.get('order_by', None)
-
+    query = request.GET.get('q', None)
 
     if category_slug == 'all':
         products = Products.objects.all()
+    elif query:
+        products = q_search(query)
     else:
         products = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
@@ -19,7 +22,7 @@ def catalog(request, category_slug):
 
     paginator = Paginator(products, 3)
     current_page = paginator.page(int(page))
-
+ 
     context = {
         "title": "DAGMeet - Каталог",
         "products": current_page,
